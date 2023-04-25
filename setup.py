9eb4161751c3138cec84ed6f2d661f6a4ff34785
LuggiStruggi <lukasmkoenig@gmx.net>
2023-04-25 13:09:08 +0200

    setuo

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 from frascii import __version__
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(	
 		name='frascii', version=__version__,
 		url='https://github.com/LuggiStruggi/Frascii.git',
 		author='Lukas König',
 		author_email='lukasmkoenig@gmx.net',
 		packages=find_packages(),
+		long_description=long_description,
+    	long_description_content_type='text/markdown',
 		entry_points={'console_scripts': ['frascii=frascii.commands:cmd_main']},
 		decription="DESCRIPTION"
 	)
