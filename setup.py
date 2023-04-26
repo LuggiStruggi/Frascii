7f81fb86d7d98264bf57ae3a430f1f7352a8cbc0
LuggiStruggi <lukasmkoenig@gmx.net>
2023-04-26 09:43:31 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 from frascii import __version__
 
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+def readme():
+    with open('README.md', 'r', encoding="utf-8") as f:
+        content = f.read()
+    return content
 
 setup(	
 		name='frascii', version=__version__,
@@ -11,7 +12,7 @@ setup(
 		author='Lukas König',
 		author_email='lukasmkoenig@gmx.net',
 		packages=find_packages(),
-		long_description=long_description,
+		long_description=readme(),
     	long_description_content_type='text/markdown',
 		entry_points={'console_scripts': ['frascii=frascii.commands:cmd_main']},
 		decription="DESCRIPTION"
